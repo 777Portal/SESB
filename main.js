1d0114f00ae578c7e0bcb5adbc64fb5363e5cca9
exon <throwawayact69xd@gmail.com>
2025-05-21 12:12:34 -0400

    Added quote, messages commands

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 import { getToken } from "./login.js";
 let token = await getToken(process.env.USERNAME, process.env.PASSWORD);
 
+
+import { getUsers, initJson, logMessage, saveMessages } from "./features/logger.js";
+await initJson();
+
 import { execSync } from 'child_process';
 const revision = execSync('git rev-parse --short HEAD').toString().trim();
 
@@ -21,8 +25,12 @@ socket.on("connect_error", (err) => {
     console.log(err.context);
 });
 
+setInterval(() => {
+    saveMessages();
+}, 10000);
+
 socket.on("connect", () => {
-    socket.emit("message", "REV."+revision);
+    socket.emit("message", "REV."+revision+" | =help");
 });
 
 socket.on("disconnect", (reason, details) => {
@@ -34,6 +42,8 @@ socket.on ("error", (err) => {
 })
 
 socket.on("message", async (message) => {
+    logMessage(message);
+    
     console.log(message);
     if (message.text.includes("=")) {
         let ror = message.text.split(" ");
@@ -42,13 +52,45 @@ socket.on("message", async (message) => {
         let args = ror[1];
         console.log(args)
 
-        if (command == "=help" ) return socket.emit("message", `SESB REV.${revision} | current commands: =search, =help`)
+        if (command == "=help" ) return socket.emit("message", `SESB REV.${revision} | current commands: =search, =help, =messages, =quote`)
 
         if (command == "=search" ) {
             let result = await getSummarizationOfQuery(message.text);
             return socket.emit("message", result.substring(0, 500))
         }
+
+        if (command == "=messages" ) {
+            let users = getUsers();
+            let user = users[args];
+            console.log(users, args)
+            if (!user) return socket.emit("message", "couldn't find user " + args + "...")
+            let messages = Object.keys(user.messages).length;
+            return socket.emit("message", "Found "+messages+" from user " + args)
+        }
+        if (command == "=quote") {
+            let users = getUsers();
+            let user = users[args.trim()];
+            if (!user) return socket.emit("message", "couldn't find user " + args + "...");
+        
+            let messageIds = Object.keys(user.messages);
+            if (messageIds.length === 0) return socket.emit("message", "No messages found for user " + args);
+        
+            let randomId = messageIds[Math.floor(Math.random() * messageIds.length)];
+            let randomMessage = user.messages[randomId];
+        
+            let formattedDate = new Date(randomMessage.timestamp).toString();
+            return socket.emit(
+                "message",
+                `${randomMessage.text} - ${randomMessage.fromUser} (${formattedDate})`
+            );
+        }
     }
+    process.on('exit', function(){ 
+        socket.emit("message", "process exited.");
+        saveMessages();
+    });
 });
+
+
 
 console.log('What kinda idiot would log his own token... heh...')
