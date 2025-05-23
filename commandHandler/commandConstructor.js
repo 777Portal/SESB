c07da3d873f25e05786a1df0a1350be249cc0a5e
exon <throwawayact69xd@gmail.com>
2025-05-23 11:16:26 -0400

    less logging, better error message for arguments

--- commandHandler/commandConstructor.js
+++ commandHandler/commandConstructor.js
@@ -14,8 +14,6 @@ export class Command {
     }
     matches (message){
         const text = message.text;
-
-        console.log("runCommand input:", typeof text, text, text.startsWith(this.prefix), this.prefix);
         if (!text.startsWith(this.prefix)) return false;
 
         let trimmed = text.substring(this.prefix.length).toLowerCase();
@@ -26,7 +24,7 @@ export class Command {
             let providedArgs = argsString.length ? argsString.split(' ') : [];
 
             if ( !(trimmed === cmd || trimmed.startsWith(cmd + ' ')) ) continue;           
-            if ( this.args.length !== providedArgs.length ) return {matches:false, feedback:`incorrect length of arguments. ${this.prefix}${cmd}${arrayStringFormat(this.args)}`};
+            if ( this.args.length !== providedArgs.length ) return {matches:false, feedback:`incorrect length of arguments. correct usage ( ${this.prefix}${cmd}${arrayStringFormat(this.args)} )`};
 
             return {matches: true, arguments: trimmed.substring(cmd.length).trim().split(' ')};
         }
