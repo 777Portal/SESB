74d79baf4df1baa638c884eea20e04fdde27a3a7
exon <throwawayact69xd@gmail.com>
2025-05-23 16:10:48 -0400

    only show commands if user has permisions to use.

--- commandHandler/commands/help.js
+++ commandHandler/commands/help.js
@@ -2,10 +2,11 @@ import { Command } from "../commandConstructor.js";
 import { getCommands } from "../commandHandler.js";
 import { getSocket } from "../../socket.js";
 import { getRevision, arrayStringFormat, getCurrentRevision } from "../../util.js";
-function helpCallback(){
+function helpCallback(message){
     let string = "Help- Prefix: ["+process.env.PREFIX+"] commands:"
     let cmds = getCommands();
     for (let command of cmds){
+        if ( !command.checkPermissions(message.fromUser).matches ) continue;
         string+= ` ${command.prefix}${command.name}${arrayStringFormat(command.args)},\n`
     }
     getSocket()?.emit("message", string);
