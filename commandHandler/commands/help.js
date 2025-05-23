47610ffcc793e0cca3126936fe0a9fd1b8ad8803
exon <throwawayact69xd@gmail.com>
2025-05-23 11:16:36 -0400

    Dynamic help command

--- commandHandler/commands/help.js
+++ commandHandler/commands/help.js
@@ -1,8 +1,14 @@
 import { Command } from "../commandConstructor.js";
+import { getCommands } from "../commandHandler.js";
 import { getSocket } from "../../socket.js";
-import { getRevision, getCurrentRevision } from "../../util.js";
+import { getRevision, arrayStringFormat, getCurrentRevision } from "../../util.js";
 function helpCallback(){
-    getSocket()?.emit("message", `SESB REV.${getRevision()} | current commands:  =help, =users, =messagecount, =topm, =search [bing search query], =messages [username], =message [username] [index], =quote [username], =firstseen [username], =lastseen [username]`)
+    let string = "Help- Prefix: ["+process.env.PREFIX+"] commands:"
+    let cmds = getCommands();
+    for (let command of cmds){
+        string+= ` ${command.prefix}${command.name}${arrayStringFormat(command.args)},\n`
+    }
+    getSocket()?.emit("message", string);
 }
 
 export const help = new Command(
