6758fd84ad16e686518729d36c22cbb9370a7af5
exon <throwawayact69xd@gmail.com>
2025-05-23 22:11:51 -0400

    forgot to convert socket => getSocket in message function

--- commandHandler/commands/message.js
+++ commandHandler/commands/message.js
@@ -10,7 +10,7 @@ function callback(username, index){
     if (!user) return socket.emit("message", "I haven't seen " + username + " yet!");
 
     let messageIds = Object.keys(user.messages);
-    if ( messageIds.length === 0 ) return socket.emit("message", "No messages found for user " + username);
+    if ( messageIds.length === 0 ) return getSocket()?.emit("message", "No messages found for user " + username);
     if ( messageIds.length-1 < index || index < 0) return getSocket()?.emit("message", `Invalid index :( max index ${messageIds.length-1}, you tried ${index}` );
     
     let message = user.messages[messageIds[index]];
