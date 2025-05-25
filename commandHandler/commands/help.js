da47d86b69c47f1d9f5b886b54a2d65740d01983
exon <throwawayact69xd@gmail.com>
2025-05-24 23:17:21 -0400

    use correct toString method function in help command

--- commandHandler/commands/help.js
+++ commandHandler/commands/help.js
@@ -3,11 +3,11 @@ import { getCommands } from "../commandHandler.js";
 import { getSocket } from "../../socket.js";
 import { getRevision, arrayStringFormat, getCurrentRevision } from "../../util.js";
 function helpCallback(message){
-    let string = "Help- Prefix: ["+process.env.PREFIX+"] commands:"
+    let string = "Help- Prefix: ["+process.env.PREFIX+"] \ncommands:\n"
     let cmds = getCommands();
     for (let command of cmds){
         if ( !command.checkPermissions(message.fromUser).matches ) continue;
-        string+= ` ${command.prefix}${command.name}${arrayStringFormat(command.args)},\n`
+        string += command.toString()+"\n";
     }
     getSocket()?.emit("message", string);
 }
