4b3ba3db6680db8924f11cba7cb2167b8f4004ce
exon <throwawayact69xd@gmail.com>
2025-05-27 11:27:09 -0400

    update purge.js

--- purge.js
+++ purge.js
@@ -4,10 +4,12 @@ await initJson();
 let users = await getUsers();
 
 // console.log(users)
+let purgeString = "";
 for (let username in users){
     let user = users[username];
     if (user.iq == null) continue; // old accounts
     // if (!username.includes("!") && !username.includes("-")) continue;
     if (Object.keys(user.messages).length < 50) continue;
-    console.log(username.split("#")[0] + " " )
+    purgeString += `${username.split("#")[0]} `;
 }
+console.log(purgeString)
